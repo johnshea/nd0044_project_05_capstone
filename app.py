b093467ab9923fa44c843fb40b13d754f9dd2da0
John Shea <sheajohnh@gmail.com>
2022-03-25 15:56:45 -0400

    feat: Implement JWT and RBAC logic for routes

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from urllib.request import urlopen
 from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from functools import wraps
+import json
+import requests
+from jose import jwt
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -47,13 +52,153 @@ class Employee(db.Model):
     }
 
 
+AUTH0_DOMAIN = 'nd0044-project-05-capstone.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'restaurant'
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+def get_token_auth_header():
+    headers = request.headers
+    if 'Authorization' not in headers:
+        raise AuthError({
+            "success": False,
+            "error": 401,
+            "message": "auth header not found"},
+            401
+        )
+    else:
+        auth_header = headers.get('Authorization')
+        auth_info = auth_header.split()
+        if len(auth_info) < 2:
+            raise AuthError({
+                "success": False,
+                "error": 401,
+                "message": "Authorization header length is not 2"},
+                401
+            )
+        if auth_info[0].lower() != 'bearer':
+            raise AuthError({
+                "success": False,
+                "error": 401,
+                "message": "Bearer keyword not found Authorization header"},
+                401
+            )
+        token = auth_header.split()[1]
+        return token
+
+def verify_decode_jwt(token):
+    jsonurl = requests.get(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = jsonurl.json()
+
+    try:
+      unverified_header = jwt.get_unverified_header(token)
+    except:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+    
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+
+    if rsa_key:
+        try:
+            # USE THE KEY TO VALIDATE THE JWT
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description':
+                    'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            "success": False,
+            "error": 400,
+            "message": "Permissions key not found in JWT"},
+            400
+        )
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            "success": False,
+            "error": 403,
+            "message": "Permission not found"},
+            403
+        )
+
+    return True
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                token = get_token_auth_header()
+                payload = verify_decode_jwt(token)
+                check_permissions(permission, payload)
+            except AuthError as ae:
+                abort(ae.status_code)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
+
 @app.route('/', methods=['GET'])
+# @requires_auth('')
 def index():
+  # print("JWT2: ", jwt)
   return "Hello Capstone!"
 
 @app.route('/checks', methods=['GET'])
-# @requires_auth('get:checks')
-def all_checks():
+@requires_auth('read:checks')
+def all_checks(permissions):
+  print("permissions: ", permissions)
   try:
     all_checks = Check.query.order_by(Check.id).all()
 
@@ -68,8 +213,8 @@ def all_checks():
       abort(400)  
 
 @app.route('/employees', methods=['GET'])
-# @requires_auth('get:employees')
-def all_employees():
+@requires_auth('read:employees')
+def all_employees(permissions):
   try:
     all_employees = Employee.query.order_by(Employee.id).all()
 
@@ -84,9 +229,8 @@ def all_employees():
     abort(400)
 
 @app.route('/employees/<int:employee_id>', methods=['DELETE'])
-# @requires_auth('delete:employees')
-# def delete_employee(permission, employee_id):
-def delete_employee(employee_id):
+@requires_auth('delete:employees')
+def delete_employee(permissions, employee_id):
   try:
     deleted_employee = Employee.query.get(employee_id)
 
@@ -98,14 +242,14 @@ def delete_employee(employee_id):
 
     return jsonify({
       'success': True,
-      'employee_id': employee_id
+      'id': employee_id
     })
   except Exception:
     abort(400)
 
 @app.route('/employees', methods=['POST'])
-# @requires_auth('create:employees')
-def post_employees():
+@requires_auth('create:employees')
+def post_employees(permissions):
   try:
     data = request.get_json()
 
@@ -129,8 +273,8 @@ def post_employees():
     abort(400)
 
 @app.route('/employees/<int:employee_id>', methods=['PATCH'])
-# @requires_auth('patch:employees')
-def patch_employee(employee_id):
+@requires_auth('update:employees')
+def patch_employee(permissions, employee_id):
   try:
     employee_to_update = Employee.query.get(int(employee_id))
 
@@ -150,7 +294,43 @@ def patch_employee(employee_id):
 
     return jsonify({
       'success': True,
-      'employee_id': employee_id
+      'id': employee_id
     })
   except Exception as e:
     abort(400)
+
+@app.errorhandler(403)
+def auth_error_403(error):
+    return jsonify({
+        "success": False,
+        "error": 403,
+        "message": "Forbidden"
+    }), 403
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "Unprocessable"
+    }), 422
+
+
+@app.errorhandler(404)
+def auth_error_404(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Not Found"
+    }), 404
+
+
+@app.errorhandler(401)
+def auth_error_401(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Unauthorized"
+    }), 401
+
