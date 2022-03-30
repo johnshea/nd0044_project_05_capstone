a738b4fb146e604006f998e1ff2139f54a51c854
John Shea <sheajohnh@gmail.com>
2022-03-29 22:27:31 -0400

    Add logic for db URI for SQLAlchemy

--- models.py
+++ models.py
@@ -2,7 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 database_path = os.environ['DATABASE_URL']
-
+if database_path.startswith("postgres://"):
+  database_path = database_path.replace("postgres://", "postgresql://", 1)
+  
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
